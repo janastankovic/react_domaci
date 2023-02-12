2c4d79485a8716903c77b1bc50ccedca9048a02d
Jana Stankovic <janastankovic7@gmail.com>
2023-02-12 16:40:49 +0100

    paginacija dodavanje na korpu

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,15 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ShopContext } from "../../context/shop-context";
 import { PRODUCTS } from "../../products";
 import { CartItem } from "./cart-item";
 import { useNavigate } from "react-router-dom";
-
 import "./cart.css";
+import Paginacija from "../../components/paginacija/paginacija";
+
+
 export const Cart = () => {
   const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
+  const [currentPage, setCurrentPage] = useState(1);
 
   const navigate = useNavigate();
+
+  const proizvodiKolica = [];
+    for (const product of PRODUCTS) {
+        if (cartItems[product.id] !== 0) {
+            proizvodiKolica.push(product);
+            console.log(product.id);
+        }
+    }
+
+
+  const BROJ_PROIZVODA_PO_STRANI = 3;
+  const pages = Math.ceil(proizvodiKolica.length / BROJ_PROIZVODA_PO_STRANI);
+  const firstIndex = (currentPage - 1) * BROJ_PROIZVODA_PO_STRANI;
+  const lastIndex = currentPage * BROJ_PROIZVODA_PO_STRANI;
+  const prikazaniProizvodi = proizvodiKolica.slice(firstIndex, lastIndex);
 
   return (
     <div className="cart">
@@ -17,7 +35,7 @@ export const Cart = () => {
         <h1>Proizvodi u korpi</h1>
       </div>
       <div className="cart">
-        {PRODUCTS.map((product) => {
+        {prikazaniProizvodi.map((product) => {
           if (cartItems[product.id] !== 0) {
             return <CartItem data={product} />;
           }
@@ -41,6 +59,9 @@ export const Cart = () => {
       ) : (
         <h1> Tvoja korpa je prazna</h1>
       )}
+       <Paginacija pages={pages} currentPage={currentPage} setCurrentPage={setCurrentPage}  />
     </div>
+    
   );
+ 
 };
